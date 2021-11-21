0c7537a370365b3b39637caa6ff667207669c0f6
Birgith Maria Treier <btreier@tktk.ee>
2021-11-21 17:11:43 +0200

    add new projects

---
+++ UserNameData/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace UserNameData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajalt sisestada oma eesnime
+            //programm kuvab kasutaja eesnime pikkust
+            //programm kuvab kasutaja eesnime esimest tähte
+            //programm kuvab kasutaja eesnime tagurpidi
+            //main meetodis ei tohi olla rohkem kui kolm rida koodi
+
+            Console.WriteLine("Sisesta oma eesnimi:");
+            string UserName = Console.ReadLine();
+
+            GetUserNameData(UserName);
+        }
+
+
+
+        public static void GetUserNameData(string userInput)
+        {
+            Console.WriteLine($"Sinu nimi on {userInput.Length} sümbolit pikk.");
+            Console.WriteLine($"Sinu eesnime esimene sümbol on {userInput[0]}");
+
+            for (int i = userInput.Length - 1; i >= 0; i--)
+            {
+                Console.Write(userInput[i]);
+            }
+
+
+
+        }
+    }
+}
